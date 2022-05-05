44e6b50d735ef5725a4da93efc99c3e2dbd96472
Casper Thule <c.thulehansen@live.com>
2022-05-05 08:42:49 +0200

    ?

--- HSFMIInterface_out.hs
+++ HSFMIInterface_out.hs
@@ -1,36 +1,41 @@
 {-# LINE 1 "HSFMIInterface.hs" #-}
 {-# LINE 1 "HSFMIInterface.hsc" #-}
-module HSFMIInterface.hsc where
+module HSFMIInterface where
+import Foreign.Storable
+import Foreign (Ptr, nullPtr, FunPtr, StablePtr)
+import Foreign.C.Types (CInt)
+import Foreign.C.String
+import Control.Monad (ap)
 
 
 
 type CompEnvT = Ptr ()
 type FMIStatus = CInt
+type FMIType = CInt
 
-{-# LINE 9 "HSFMIInterface.hsc" #-}
 data Status = OK | Warning | Discard | Error | Fatal | Pending deriving (Enum, Show)
 type CallbackLogger = FunPtr(CompEnvT -> CString -> FMIStatus -> CString -> CString -> IO ())
 type StepFinished = FunPtr(CompEnvT -> FMIStatus -> IO ())
 data CallbackFunctions = CallbackFunctions {
   logger :: CallbackLogger,
-  allocMem :: Ptr(),
+  allocMem :: Ptr(), -- Ignoring
   freeMem :: Ptr (), -- Ignoring
-  stepFinished :: StepFinished, -- Ignoring
+  stepFinished :: StepFinished,
   compEnv :: CompEnvT
   }
 
 instance Storable CallbackFunctions where
   sizeOf _ = ((40))
-{-# LINE 22 "HSFMIInterface.hsc" #-}
+{-# LINE 27 "HSFMIInterface.hsc" #-}
   alignment _ = (8)
-{-# LINE 23 "HSFMIInterface.hsc" #-}
+{-# LINE 28 "HSFMIInterface.hsc" #-}
   peek p = return CallbackFunctions
     `ap` ((\hsc_ptr -> peekByteOff hsc_ptr 0) p)
-{-# LINE 25 "HSFMIInterface.hsc" #-}
-    `ap` nullPtr
-    `ap` nullPtr
+{-# LINE 30 "HSFMIInterface.hsc" #-}
+    `ap` return nullPtr
+    `ap` return nullPtr
     `ap` ((\hsc_ptr -> peekByteOff hsc_ptr 24) p)
-{-# LINE 28 "HSFMIInterface.hsc" #-}
+{-# LINE 33 "HSFMIInterface.hsc" #-}
     `ap`  ((\hsc_ptr -> peekByteOff hsc_ptr 32) p)
-{-# LINE 29 "HSFMIInterface.hsc" #-}
+{-# LINE 34 "HSFMIInterface.hsc" #-}
 
